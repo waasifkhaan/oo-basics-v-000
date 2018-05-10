bc0d54643176240f39ab0c35a380b14fcedb437e
Wasif Khan <waasif.khaan@gmail.com>
2018-05-10 16:23:24 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,11 @@
 class Book
-  attr_accessor :title,  :author, :genre , :page_count, :turn_page
+  attr_accessor :title,  :author, :genre , :page_count
   def initialize(title)
     @title = title
     end
+  def turn_page
+  
+  end
+  
   
 end
