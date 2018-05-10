481148efa03b7ef7c5de2d4878f700f3fbe40d74
Wasif Khan <waasif.khaan@gmail.com>
2018-05-10 16:26:05 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 class Book
-  attr_accessor :title,  :author, :genre , :page_count
+  attr_accessor :author, :genre , :page_count
   def initialize(title)
     @title = title
     end
   def turn_page
-  
+    puts "Flipping the page...wow, you read fast!"
   end
   
   
